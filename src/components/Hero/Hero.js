e94c177421203349969a8d7aec4cdbe0eaa10551
samir jadhav <samirzjadhav@gmail.com>
2024-10-26 13:19:19 +0200

    feat: added new animation on headphone section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,6 +3,38 @@ import Headphone1 from "../../assets/headphone.png";
 import Headphone2 from "../../assets/headphone2.png";
 import Headphone3 from "../../assets/headphone3.png";
 import { IoReturnUpBack } from "react-icons/io5";
+import { FaWhatsapp } from "react-icons/fa";
+import { UpdateFollower } from "react-mouse-follower";
+import { AnimatePresence, motion } from "framer-motion";
+
+const fadeUp = (delay) => {
+  return {
+    hidden: {
+      opacity: 0,
+      y: 100,
+      scale: 0.5,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      scale: 1,
+      transition: {
+        duration: 0.5,
+        delay: delay,
+        ease: "easeInOut",
+      },
+    },
+    exit: {
+      opacity: 0,
+      y: 50,
+      scale: 0.5,
+      transition: {
+        duration: 0.2,
+        ease: "easeInOut",
+      },
+    },
+  };
+};
 
 const headphoneData = [
   {
@@ -42,6 +74,11 @@ const headphoneData = [
 
 const Hero = () => {
   const [currentHeadphone, setCurrentHeadphone] = useState(headphoneData[0]);
+
+  const handleCurrentHeadphone = (data) => {
+    setCurrentHeadphone(data);
+  };
+
   return (
     <>
       <section className="bg-brandDark text-white">
@@ -49,18 +86,59 @@ const Hero = () => {
           {/* Headphone Info */}
           <div className="flex flex-col justify-center py-14 md:py-0 xl:max-w-[500px]">
             <div className="space-y-5 text-center md:text-left ">
-              <h1 className="text-3xl lg:text-6xl font-bold font-varela">
-                {currentHeadphone.title}
-              </h1>
-              <p className="text-sm leading-loose text-white/80">
-                {currentHeadphone.subtitle}
-              </p>
-              <button
-                style={{ backgroundColor: currentHeadphone.bgColor }}
-                className="px-4 py-2 inline-block font-normal rounded-sm"
-              >
-                Buy and Listen
-              </button>
+              <AnimatePresence mode="wait">
+                <UpdateFollower
+                  mouseOptions={{
+                    backgroundColor: "white",
+                    zIndex: 9999,
+                    followSpeed: 0.5,
+                    rotate: -720,
+                    mixBlendMode: "difference",
+                    scale: 10,
+                  }}
+                >
+                  <motion.h1
+                    key={currentHeadphone.id}
+                    variants={fadeUp(0.2)}
+                    initial="hidden"
+                    animate="show"
+                    exit="exit"
+                    className="text-3xl lg:text-6xl font-bold font-varela"
+                  >
+                    {currentHeadphone.title}
+                  </motion.h1>
+                </UpdateFollower>
+              </AnimatePresence>
+
+              <AnimatePresence mode="wait">
+                {" "}
+                <motion.p
+                  key={currentHeadphone.id}
+                  variants={fadeUp(0.3)}
+                  initial="hidden"
+                  animate="show"
+                  exit="exit"
+                  className="text-sm leading-loose text-white/80"
+                >
+                  {currentHeadphone.subtitle}
+                </motion.p>
+              </AnimatePresence>
+
+              <UpdateFollower>
+                <AnimatePresence mode="wait">
+                  <motion.button
+                    key={currentHeadphone.id}
+                    variants={fadeUp(0.3)}
+                    initial="hidden"
+                    animate="show"
+                    exit="exit"
+                    style={{ backgroundColor: currentHeadphone.bgColor }}
+                    className="px-4 py-2 inline-block font-normal rounded-sm"
+                  >
+                    Buy and Listen
+                  </motion.button>
+                </AnimatePresence>
+              </UpdateFollower>
               {/* HEADPHONE LIST SEPARATOR */}
               <div className="flex items-center justify-center md:justify-start gap-4 !mt-24">
                 <div className="w-20 h-[1px] bg-white"></div>
@@ -71,7 +149,11 @@ const Hero = () => {
               <div className="grid grid-cols-3 gap-10">
                 {headphoneData.map((item) => {
                   return (
-                    <div className="grid grid-cols-2 place-items-center cursor-pointer gap-[10px]">
+                    <div
+                      key={item.id}
+                      onClick={() => handleCurrentHeadphone(item)}
+                      className="grid grid-cols-2 place-items-center cursor-pointer gap-[10px]"
+                    >
                       <div>
                         <img
                           src={item.image}
@@ -93,7 +175,20 @@ const Hero = () => {
           </div>
           {/* Hero Image */}
 
+          <div className="flex flex-col justify-end items-center">
+            <img
+              src={currentHeadphone.image}
+              alt={currentHeadphone.alt}
+              className="w-[300px] md:w-[400px] xl:w-[550px]"
+            />
+          </div>
+
           {/* WhatApp Icon */}
+          <div className="text-3xl text-white fixed botttom-10 right-10 hover:rotate-[360deg] duration-500 z-[99999] mix-blend-difference">
+            <a href="">
+              <FaWhatsapp />
+            </a>
+          </div>
         </div>
       </section>
     </>
