bbe291b0b40f12baabd6663a1ab5dcdf243b5004
samir jadhav <samirzjadhav@gmail.com>
2024-11-04 15:08:22 +0530

    feat: added new information for headphone title desc icons dealy

---
+++ src/components/Services/Service.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Icon1 from "../../assets/icons/obj1.png";
+import Icon2 from "../../assets/icons/obj2.png";
+import Icon3 from "../../assets/icons/obj3.png";
+import Icon4 from "../../assets/icons/obj4.png";
+
+const serviceData = [
+  {
+    id: 1,
+    icon: Icon1,
+    title: "Security",
+    desc: "We provide the best security for your data and information. Our security measures are top-notch and we ensure that your data is safe with us.",
+    delay: 0.5,
+  },
+  {
+    id: 2,
+    icon: Icon2,
+    title: "Gurantee",
+    desc: "gurantee your data is safe with us. We provide the best security for your data and information. Our security measures are top-notch and we ensure that your data is safe with us.",
+    delay: 0.5,
+  },
+  {
+    id: 3,
+    icon: Icon3,
+    title: "Affordability",
+    desc: "Affordable prices for our services. We provide the best security for your data and information. Our security measures are top-notch and we ensure that your data is safe with us.",
+    delay: 0.5,
+  },
+];
+
+const Service = () => {
+  return <></>;
+};
+
+export default Service;
